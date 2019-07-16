797e511eacfff385920c3a72868a4c9256131a90
Anand Singh <anand4carcility@gmail.com>
2019-07-16 06:12:49 +0530

    Updates

---
+++ precache-manifest.abf7ece99e8c7573e9ee1b582bd50395.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fe15b821ce1ec9e591a05f27efd023a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "99075c1bdcedef5c3a64",
+    "url": "/monsters-rolodex/static/css/main.b3757b35.chunk.css"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/monsters-rolodex/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "99075c1bdcedef5c3a64",
+    "url": "/monsters-rolodex/static/js/main.a2b8c850.chunk.js"
+  },
+  {
+    "revision": "49006584ddd3bfbd8db8",
+    "url": "/monsters-rolodex/static/js/runtime~main.26edc6c9.js"
+  }
+]);
